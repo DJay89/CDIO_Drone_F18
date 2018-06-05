0ca664cd9f9bedf3f2bcb094c0be0da025102346
Coci0alex <alexanderCS1@hotmail.com>
2018-06-05 14:38:26 +0200

    Testing level zero of search algo

--- src/Video/Main.java
+++ src/Video/Main.java
@@ -4,20 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import managers.PilotManager;
+import managers.QRsearch;
 import object_recogniztion.image_recogniztion.ImageRecognition;
 
-public class Main extends Application {
+public class main extends Application {
 
     private VideoDisplay vd;
     private PilotManager pm;
     private ImageRecognition IR;
     private Thread imgThread;
+    private QRsearch qr;
     private Boolean devMode = false;
 
     public void start(Stage s){
         pm = new PilotManager();
         vd = new VideoDisplay(pm);
         IR = new ImageRecognition(pm);
+        qr = new QRsearch(IR);
         vd.start(s);
         while (pm.getImg() == null && devMode == false) {
             // wait for camera
@@ -26,6 +29,13 @@ public class Main extends Application {
         System.out.println("Camera ready");
         imgThread = new Thread(IR);
         imgThread.start();
+
+        pm.hover(2000);
+        if ( qr.searchLvlZero() == 1 )
+        pm.land();
+
+
+
 
     }
     public static void main(String [] args)
