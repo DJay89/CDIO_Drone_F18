2b0eb246feb9fb572cecf8ce2914dd7d19d2a407
Coci0alex <alexanderCS1@hotmail.com>
2018-06-05 11:16:17 +0200

    Added QR search

---
+++ src/managers/QRsearch.java
@@ -0,0 +1,38 @@
+package managers;
+
+import de.yadrone.base.command.CommandManager;
+
+import java.util.ArrayList;
+
+public class QRsearch {
+
+    private CommandManager cmd;
+    private long time = 5000;
+    public QRsearch() {
+
+    }
+
+    public void searchLvlZero() {
+
+        ArrayList<Integer> QRresult = new ArrayList<Integer>();
+
+        //// RUN QR CODE LISTENER
+        //cmd.hover(5000);
+
+   /*     if (  qr not localized ) {
+            cmd.spinLeft(time);
+        }
+
+        else {
+            // SCAN QR and save result in list
+
+        }
+        */
+    }
+
+
+    public void searchLvlOne() {
+
+    }
+
+}
