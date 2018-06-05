c96ab19d4b103a49315e78b3ea69dced173dbc9e
Coci0alex <alexanderCS1@hotmail.com>
2018-06-05 13:38:59 +0200

    Added Different Search Level

--- src/managers/QRsearch.java
+++ src/managers/QRsearch.java
@@ -8,30 +8,67 @@ public class QRsearch {
 
     private CommandManager cmd;
     private long time = 5000;
+    private int resultQR = 0;
+    private int result = -1;
+
     public QRsearch() {
 
     }
 
-    public void searchLvlZero() {
+    public boolean searchLvlZero() {
 
-        ArrayList<Integer> QRresult = new ArrayList<Integer>();
 
         //// RUN QR CODE LISTENER
-        //cmd.hover(5000);
+        cmd.hover();
 
-   /*     if (  qr not localized ) {
+       if (  qr not localized ) {
             cmd.spinLeft(time);
         }
 
         else {
             // SCAN QR and save result in list
 
+            if ( result+1 == QRresult )
+            {
+                result = QRresult;
+                // fly through ring //
+            }
+
+            else {
+                searchLvlOne();
+            }
+
         }
-        */
+
     }
 
 
     public void searchLvlOne() {
+
+
+        // RUN QR CODE LISTENER //
+
+        if ( qr not localized ) {
+            // generate random number and fly according to these pin points. spin 360 at every stop.
+
+        }
+
+        else {
+            // SCAN QR and save result in list
+            if ( result+1 == QRresult )
+            {
+                result = QRresult;
+                // fly through ring //
+            }
+
+            else {
+                    searchLvlTwo();
+            }
+        }
+    }
+
+
+    public boolean searchLvlTwo() {
 
     }
 
