90103b06f4842a6f4542be7f99a8fdaff0f5cf81
JakobsenSWT <Jakobsen1608@hotmail.com>
2018-06-11 11:19:01 +0200

    RedRing improvements

---
+++ src/object_recogniztion/misc/SonarSearch.java
@@ -0,0 +1,14 @@
+package object_recogniztion.misc;
+
+public class SonarSearch implements Runnable {
+
+    private int minRange;
+    private int maxRange;
+
+    public void SonarSearch () {    }
+
+    @Override
+    public void run() {
+
+    }
+}
